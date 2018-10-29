08b11cd4e49308dbcf55df57a55b28126002759f
Gray Smith <gray.smith00@gmail.com>
2018-10-29 10:58:30 -0600

    Initial commit

---
+++ caaAgencyScrape.js
@@ -0,0 +1,36 @@
+const Nightmare = require('nightmare');
+const nightmare = Nightmare({ show: true });
+
+const fs = require('fs-extra');
+
+nightmare
+  .goto('https://www.caa.com/entertainmenttalent/touring/search#electronic')
+  .wait(10000)
+  .evaluate(() => window.scrollTo(0, document.body.scrollHeight))
+  .wait(10000)
+  .evaluate(() => window.scrollTo(0, document.body.scrollHeight))
+  .wait(10000)
+  .evaluate(() => window.scrollTo(0, document.body.scrollHeight))
+  .wait(10000)
+  .evaluate(() => window.scrollTo(0, document.body.scrollHeight))
+  .wait(10000)
+  .evaluate(() => window.scrollTo(0, document.body.scrollHeight))
+  .evaluate(() => {
+    // window.scrollTo(0, document.body.scrollHeight);
+    const artistTags = [...document.querySelectorAll('.artist-name')];
+    const artistNames = artistTags.map(tag => tag.innerText);
+    return artistNames;
+  })
+  .end()
+  .then(result => {
+    fs.writeFile(
+      'caaArtistData.json',
+      JSON.stringify(result, null, '\t'),
+      function(err) {
+        if (!err) {
+          console.log('file saved!');
+        }
+      }
+    );
+  })
+  .catch(err => console.log('oooops something went wrong: ', err));
---
+++ utaAgencyScrape.js
@@ -0,0 +1,45 @@
+const Nightmare = require('nightmare');
+const nightmare = Nightmare({ show: true });
+
+const fs = require('fs-extra');
+
+nightmare
+  .goto('https://music.utatouring.com/full-roster/')
+  .wait('#charZ')
+  .evaluate(() => {
+    const artistTags = [...document.querySelectorAll('a')].slice(30);
+    const nonArtistVals = [
+      ...'123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
+      'Worldwide Roster',
+      'UNITED STATES / CANADA AGENT FINDER',
+      'OFFICES',
+      'LONDON',
+      'LOS ANGELES',
+      'MALMÖ',
+      'MIAMI',
+      'NASHVILLE',
+      'NEW YORK',
+      'UTA MUSIC'
+    ];
+    const artistNames = artistTags.reduce((artistNames, tag) => {
+      const notValid = nonArtistVals.find(val => val === tag.innerText);
+      if (!notValid) {
+        artistNames = [...artistNames, tag.innerText];
+      }
+      return artistNames;
+    }, []);
+    return artistNames;
+  })
+  .end()
+  .then(result => {
+    fs.writeFile(
+      'utaArtistData.json',
+      JSON.stringify(result, null, '\t'),
+      function(err) {
+        if (!err) {
+          console.log('file saved!');
+        }
+      }
+    );
+  })
+  .catch(err => console.log('oooops something went wrong: ', err));
